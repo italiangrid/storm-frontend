94d1d6a057ac19e0c993ea47ba772bddd3723cd8
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-03-10 10:02:32 +0000

    Sone formal changes of environment variables...

--- frontend-setup.sh
+++ frontend-setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 export GLOBUS_LOCATION=/opt/globus
 export GPT_LOCATION=/opt/gpt
+export GLITE_LOCATION=/opt/glite
 export GSOAPGSI_DIR=/usr
-export GSOAP_DIR=/usr/local/gsoap-2.6/soapcpp2
+export GSOAP_DIR=/usr/local/gsoap-2.7/bin
 export MYSQL_DIR=/usr
 export MYSQLAPI_DIR=/opt/mysql++
 export XMLRPC_C_DIR=/home/storm/xmlrpc-c_new
-#
+export STORM_DIR=/home/storm
+export PATH=$PATH:$XMLRPC_C_DIR/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLITE_LOCATION/lib
