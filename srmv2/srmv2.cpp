ca080dffc7cffc14fde8a26605b8b5046904839a
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-11-27 16:12:40 +0000

    fix

--- srmv2/srmv2.cpp
+++ srmv2/srmv2.cpp
@@ -317,7 +317,7 @@ static int srm_main(struct main_args *main_args) {
         flags |= CGSI_OPT_DISABLE_NAME_CHECK;
         srmlogit(STORM_LOG_NONE, func, "Name check disabled\n");
     }
-    soap_register_plugin_arg(soap_data, server_cgsi_plugin, &flags);i
+    soap_register_plugin_arg(soap_data, server_cgsi_plugin, &flags);
 #endif
     soap_data->bind_flags |= SO_REUSEADDR;
 
