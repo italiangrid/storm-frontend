1fce4d0dc1d873d1a11e424d5ebb9a2ac78f3331
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-12-11 10:44:17 +0000

    devel

--- src/frontend/FrontendConfiguration.cpp
+++ src/frontend/FrontendConfiguration.cpp
@@ -362,7 +362,7 @@ void FrontendConfiguration::checkCreateDir(string dirAbsolutePath) {
     }
 
     if (access(dirAbsolutePath.c_str(), W_OK| X_OK) != 0) {
-        if ((!S_IWOTH(statInfo.st_mode)) && (!S_IXOTH(statInfo.st_mode))) {
+        if (!((statInfo.st_mode & S_IWOTH) && (statInfo.st_mode & S_IXOTH))) {
             throw runtime_error("Permission denied (\"wx\" is needed) for directory \""
                     + dirAbsolutePath + "\". "
                 "Currently running as user \"" + user + "\".");
