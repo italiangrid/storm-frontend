659b354e0174a94c726f16cc3e6c9f4055ff14d6
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-12-12 09:42:26 +0000

    removed commented code

--- src/frontend/ptp.cpp
+++ src/frontend/ptp.cpp
@@ -317,11 +317,6 @@ void ptp::insert(struct srm_dbfd *db) {
             _surls.at(i).status = SRM_USCOREFAILURE;
             _surls.at(i).explanation = "None of the requested transfer protocols is supported";
         }
-//        for (std::vector<ptp::surl_t>::const_iterator i = _surls.begin(); i != _surls.end(); ++i) {
-//            i->status = static_cast<ns1__TStatusCode>(SRM_USCOREFAILURE);
-//            i->explanation = "None of the requested transfer protocols is supported";
-//        }
-
 
         throw storm::not_supported("None of the requested transfer protocols is supported");
     }
