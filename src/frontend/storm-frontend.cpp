d21b663985183481fdf7852aab6630712793ad57
Alberto Forti <alberto.forti@cnaf.infn.it>
2008-12-15 15:59:40 +0000

    devel

--- src/frontend/storm-frontend.cpp
+++ src/frontend/storm-frontend.cpp
@@ -318,9 +318,9 @@ int main(int argc, char** argv)
 }
 
 void *
-process_request(void *soap) {
-
-    soap->user = (struct srm_srv_thread_info*) mysql_connection_pool->getConnection(boost::this_thread::get_id());
+process_request(void *soap_vp) {
+    struct soap* soap = soap_vp;
+    soap->user = mysql_connection_pool->getConnection(boost::this_thread::get_id());
     soap->recv_timeout = SOAP_RECV_TIMEOUT;
     soap->send_timeout = SOAP_SEND_TIMEOUT;
 
