ce8334a96fc1de1751ef7ca139382c604f544cad
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-06-04 14:35:09 +0000

    devel

--- src/frontend/ping.cpp
+++ src/frontend/ping.cpp
@@ -1,13 +1,17 @@
 #include "Monitoring.hpp"
 #include "srmv2H.h"
 
-extern "C" int ns1__srmPing_impl(struct soap* soap, struct ns1__srmPingRequest *req, struct ns1__srmPingResponse_ *rep);
+extern "C" int ns1__srmPing_impl(struct soap* soap, struct ns1__srmPingRequest *req,
+        struct ns1__srmPingResponse_ *rep);
 
-int ns1__srmPing(struct soap* soap, struct ns1__srmPingRequest *req, struct ns1__srmPingResponse_ *rep)
-{
+int ns1__srmPing(struct soap* soap, struct ns1__srmPingRequest *req, struct ns1__srmPingResponse_ *rep) {
     int ret_val = ns1__srmPing_impl(soap, req, rep);
 
-    storm::Monitoring::getInstance()->notifyPingCompleted(-1, true);
+    if (ret_val != SOAP_OK) {
+        storm::Monitoring::getInstance()->notifyPingCompleted(-1, false);
+    } else {
+        storm::Monitoring::getInstance()->notifyPingCompleted(-1, true);
+    }
 
     return ret_val;
 }
