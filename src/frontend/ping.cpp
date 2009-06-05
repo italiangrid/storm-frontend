19928a1dcc4f6edc63047dd705f39580325c6902
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-06-05 08:29:34 +0000

    added ping execution time measurement

--- src/frontend/ping.cpp
+++ src/frontend/ping.cpp
@@ -1,16 +1,25 @@
 #include "Monitoring.hpp"
 #include "srmv2H.h"
 
+#include <ctime>
+
 extern "C" int ns1__srmPing_impl(struct soap* soap, struct ns1__srmPingRequest *req,
         struct ns1__srmPingResponse_ *rep);
 
 int ns1__srmPing(struct soap* soap, struct ns1__srmPingRequest *req, struct ns1__srmPingResponse_ *rep) {
+
+    clock_t start_time = clock();
+
     int ret_val = ns1__srmPing_impl(soap, req, rep);
+
+    clock_t end_time = clock();
+
+    float et = (end_time - start_time) / CLOCKS_PER_SEC;
 
     if (ret_val != SOAP_OK) {
         storm::Monitoring::getInstance()->notifyPingCompleted(-1, false);
     } else {
-        storm::Monitoring::getInstance()->notifyPingCompleted(-1, true);
+        storm::Monitoring::getInstance()->notifyPingCompleted(et, true);
     }
 
     return ret_val;
