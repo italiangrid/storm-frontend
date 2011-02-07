11b2d6c7d3a082912521558e3d49b5f408712c62
Riccardo Zappi <riccardo.zappi@cnaf.infn.it>
2011-02-07 11:20:26 +0000

    merge FE_1.6 r7264 in trunk

--- src/frontend/storm-frontend.cpp
+++ src/frontend/storm-frontend.cpp
@@ -1,6 +1,17 @@
-/*
- * $Id: srmv2.c 2777 2006-08-29 13:20:17Z amessina $
- */
+/* Copyright (c) Istituto Nazionale di Fisica Nucleare (INFN). 2006-2010.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 
 /**
  * \file srmv2.c This file contains the main functions to parse
