fca4d8a9446b03906a07f045987ce037fce73027
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2013-11-04 11:12:17 +0100

    More request handling refactoring.

---
+++ src/frontend/request/synch/logging_traits.hpp
@@ -0,0 +1,26 @@
+/*
+ * logging_traits.hpp
+ *
+ *  Created on: Oct 30, 2013
+ *      Author: andrea
+ */
+
+#ifndef LOGGING_TRAITS_HPP_
+#define LOGGING_TRAITS_HPP_
+
+namespace storm {
+
+	template< typename t>
+	struct request_traits{
+		void log() const{
+
+		};
+	};
+
+
+}
+
+
+
+
+#endif /* LOGGING_TRAITS_HPP_ */
